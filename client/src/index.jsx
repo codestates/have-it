b0d085eb7bd79ec14e5ae1749d639a20fb05f714
Yunwoo Ji <unu12073@gmail.com>
2021-10-05 22:26:28 +0900

    [client] chore: Set up styled-normalize

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,9 +3,11 @@ import "react-app-polyfill/stable";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import GlobalStyle from "./styles";
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
