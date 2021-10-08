16755d9a39e5f4be7b99829527c9d123ea22abea
heegu0311 <heegu0311@gmail.com>
2021-10-09 01:43:50 +0900

    [client] feat: #10 Re-layout app for header & nav

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import styled from "styled-components";
 import Home from "./pages/Home";
 import MyPage from "./pages/MyPage";
 import Habit from "./pages/Habit";
@@ -10,35 +11,37 @@ import Modal from "./components/Modal";
 import Auth from "./components/Auth";
 import HabitCreate from "./components/HabitCreate";
 import HabitJoin from "./components/HabitJoin";
-import { habitCreateModalOnAction } from "./store/actions";
+
+const FlexColumnContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const FlexContainer = styled.div`
+  display: flex;
+`;
 
 const App = () => {
-  const dispatch = useDispatch();
   const { isAuthModal, isHabitCreateModal, isHabitJoinModal } = useSelector(
     ({ modalReducer }) => modalReducer
   );
+
   const isModal = isAuthModal || isHabitCreateModal || isHabitJoinModal;
 
   return (
     <BrowserRouter>
-      <button
-        type="button"
-        onClick={() => {
-          dispatch(habitCreateModalOnAction);
-        }}
-      >
-        habitCreateModalOnAction
-      </button>
-      <Nav />
-      <div>
-        <Header />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/mypage" component={MyPage} />
-          <Route path="/habit/:id" component={Habit} />
-          <Redirect from="*" to="/" />
-        </Switch>
-      </div>
+      <FlexContainer>
+        <Nav />
+        <FlexColumnContainer>
+          <Header />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/mypage" component={MyPage} />
+            <Route path="/habit/:id" component={Habit} />
+            <Redirect from="*" to="/" />
+          </Switch>
+        </FlexColumnContainer>
+      </FlexContainer>
       {isModal && (
         <Modal>
           {isAuthModal && <Auth />}
