e6c42e09fc5d222903a3f6eb9ea270a109116129
Yunwoo Ji <unu12073@gmail.com>
2021-10-11 20:39:39 +0900

    [client] fix: #62 Fix typo

--- client/src/api/categories.js
+++ client/src/api/categories.js
@@ -2,7 +2,7 @@ import api from "./index";
 
 const categoriesApi = {
   getCategories: () => api.get("/categories"),
-  getCategoryByEnTitle: (enTitle) => api.get(`/categories${enTitle}`),
+  getCategoryByEnTitle: (enTitle) => api.get(`/categories/${enTitle}`),
 };
 
 export default categoriesApi;
