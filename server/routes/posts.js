6f875d07418c7e6a2bff297d2406eedac31136d9
YUN-KC <wjdrlfgg@gmail.com>
2021-10-12 18:36:57 +0900

    [server]  feat: #79 Add posts api

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const posts = require("../controllers/posts");
-const { findPosts, writePost, modifyPost, removePost } = posts;
+const { writePost, modifyPost, removePost } = posts;
 const isAuth = require("../middlewares/auth");
+const multer = require("multer");
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
-router.get("/:habits_id", isAuth, findPosts);
-router.post("/:habits_id", isAuth, writePost);
-router.put("/:posts_id", isAuth, modifyPost);
+router.post("/:habits_id", isAuth, upload.single("image"), writePost);
+router.put("/:posts_id", isAuth, upload.single("image"), modifyPost);
 router.delete("/:posts_id", isAuth, removePost);
 
 module.exports = router;
